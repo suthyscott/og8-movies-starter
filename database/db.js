14b1cad95e777fb2645ff93f756b7d67f80cc573
kylebaugh <kylebaugh1@gmail.com>
2024-05-21 13:59:48 -0600

    starter code

---
+++ database/db.js
@@ -0,0 +1,25 @@
+import { Sequelize } from 'sequelize';
+
+async function connectToDB(dbURI) {
+  console.log(`Connecting to DB: ${dbURI}`);
+
+  const sequelize = new Sequelize(dbURI, {
+    logging: console.log, // set logging: false to disable outputting SQL queries to console
+    define: {
+      underscored: true,
+      timestamps: false,
+    },
+    password: 'admin'
+  });
+
+  try {
+    await sequelize.authenticate();
+    console.log('Connected to DB successfully!');
+  } catch (error) {
+    console.error('Unable to connect to DB:', error);
+  }
+
+  return sequelize;
+}
+
+export default connectToDB;
